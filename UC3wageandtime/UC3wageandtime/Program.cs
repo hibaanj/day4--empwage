5b510482eb054bd9a0190c8252b1cc9076247c33
hibaanj <anjumhiba15@gmail.com>
2022-07-17 05:52:01 +0530

    wage using parttime and full time

---
+++ UC3wageandtime/UC3wageandtime/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc3
+{
+    internal class Uc3pertime
+    {
+        public static void partime()
+        {
+            int FULLTIME = 1;
+            int Parttime = 2;
+            int emphr = 0;
+            Random random = new Random();
+            int check = random.Next(0, 3);
+            if (check == FULLTIME)
+            {
+                emphr = 12;
+                Console.WriteLine("the emp was present full time and his working hrs are : " + emphr);
+            }
+            else if (check == Parttime)
+            {
+                emphr = 8;
+                Console.WriteLine("the emp was present part time and his working hrs are : " + emphr);
+
+            }
+            else
+            {
+                emphr = 0;
+                Console.WriteLine("emp is absnnt " + emphr);
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            partime();
+        } 
+
+
+    }
+}
