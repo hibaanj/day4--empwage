ddd65727e3509d1d9f70fda7d1f3750feebf1ad9
hibaanj <anjumhiba15@gmail.com>
2022-07-17 05:30:18 +0530

    employee is present or absent

---
+++ UC1emppresentabs/UC1emppresentabs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    public class Uc1emppresent
+    {
+        public static void Emppresent()
+        {
+            int FULLTIME = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == FULLTIME)
+            {
+                Console.WriteLine("emp is present today");
+
+            }
+            else
+            {
+                Console.WriteLine("emp is absent today");
+
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            Emppresent();
+
+        }
+    }
+
+}
+
